6a3b7d5aaa98537f6bde1e9a8321612ffeb69acb
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-02 14:08:22 +0700

    Path Variable, PHP MVC

---
+++ 7-path-variable/app/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AriefKarditya\PathVariablePhp\Controller;
+
+class HomeController
+{
+    function index()
+    {
+        echo "HomeController.index()";
+    }
+
+    function hello()
+    {
+        echo "HomeController.hello()";
+    }
+
+    function world()
+    {
+        echo "HomeController.world()";
+    }
+
+    function About()
+    {
+        echo "Author: Arief Karditya Hermawan";
+    }
+}
---
+++ 7-path-variable/tests/RegexTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AriefKarditya\PathVariablePhp;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+class RegexTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function RegexTry()
+    {
+        $path = "/products/12345/categories/abcde";
+
+        $pattern = "#^/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)$#";
+
+        $result = preg_match($pattern, $path, $variables);
+
+        Assert::assertEquals(1, $result);
+
+        var_dump($variables);
+
+        array_shift($variables); # Menghapus isi array dengan index 0.
+        var_dump($variables);
+
+    }
+}
